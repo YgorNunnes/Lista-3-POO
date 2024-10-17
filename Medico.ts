75f6b53c9d4f6ba9d14f5ac7594017723f6d87db
Legotche <d4n13lb0rg35v4ll4d4r35@gmail.com>
2024-10-17 09:28:16 -0400

    Ajuste nos nomes das variaveis

--- Medico.ts
+++ Medico.ts
@@ -1,36 +1,36 @@
 export class Medico{
-    private nome: string;
-    private crm: string;
-    private email: string;
+    private _nome: string;
+    private _crm: string;
+    private _email: string;
 
-	constructor($nome: string, $crm: string, $email: string) {
-		this.nome = $nome;
-		this.crm = $crm;
-		this.email = $email;
+	constructor(nome: string, crm: string, email: string) {
+		this._nome = nome;
+		this._crm = crm;
+		this._email = email;
 	}
 
-	public get $nome(): string {
-		return this.nome;
+	public get nome(): string {
+		return this._nome;
 	}
 
-	public get $crm(): string {
-		return this.crm;
+	public get crm(): string {
+		return this._crm;
 	}
 
-	public get $email(): string {
-		return this.email;
+	public get email(): string {
+		return this._email;
 	}
 
-	public set $nome(value: string) {
-		this.nome = value;
+	public set nome(value: string) {
+		this._nome = value;
 	}
 
-	public set $crm(value: string) {
-		this.crm = value;
+	public set crm(value: string) {
+		this._crm = value;
 	}
 
-	public set $email(value: string) {
-		this.email = value;
+	public set email(value: string) {
+		this._email = value;
 	}
 
 }
